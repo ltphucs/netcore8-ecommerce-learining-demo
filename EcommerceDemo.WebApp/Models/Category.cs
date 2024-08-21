4e6ef6652c6e4219902bb0b3eda5b3e36ac4971b
ltphucs <ltphuc@gmail.com>
2024-08-21 15:38:22 +0700

    Add file CRUD Category

---
+++ EcommerceDemo.WebApp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceDemo.WebApp.Models
+{
+	public class Category
+	{
+        [Key]
+        public int Id { get; set; }
+
+		[Required]
+		[MaxLength(30)]
+        [Display(Name ="Category Name") ]
+        public string Name { get; set; }
+
+        [Display(Name="Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
+
